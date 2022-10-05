f830b2db683750774ab26bacd967c3883ca73056
Inrix <Inrixia@gmail.com>
2022-10-05 17:42:11 +1300

    Fixed prettier settings

--- src/nmea.ts
+++ src/nmea.ts
@@ -1,10 +1,10 @@
-import { createConnection } from 'net';
-import { DateTimeControl } from 'set-system-clock';
-import admin from 'admin-check';
-import args from 'args';
-import GPS from 'gps';
+import { createConnection } from "net";
+import { DateTimeControl } from "set-system-clock";
+import admin from "admin-check";
+import args from "args";
+import GPS from "gps";
 
-import type { ZDA } from 'gps';
+import type { ZDA } from "gps";
 
 const exitWithError = (err: string) => {
 	console.error(err);
@@ -13,31 +13,31 @@ const exitWithError = (err: string) => {
 
 type Flags = { host: string; port: string };
 const start = async () => {
-	args.option('host', 'The tcp ip address to connect to').option('port', 'The tcp port to connect to');
+	args.option("host", "The tcp ip address to connect to").option("port", "The tcp port to connect to");
 	const { host, port } = <Flags>args.parse(process.argv);
 
-	if (host === undefined) exitWithError('You must specify a host to connect to');
-	if (port === undefined) exitWithError('You must specify a port to connect to');
+	if (host === undefined) exitWithError("You must specify a host to connect to");
+	if (port === undefined) exitWithError("You must specify a port to connect to");
 
 	const gps = new GPS();
 	const dtc = new DateTimeControl();
-	gps.on('data', (data: ZDA) => {
+	gps.on("data", (data: ZDA) => {
 		if (data.valid) dtc.setDateTime(data.time);
 	});
 
 	console.log(`Connecting to ${host}:${port}`);
 
-	let lineBuffer = '';
+	let lineBuffer = "";
 	createConnection({ host, port: +port })
-		.on('connect', () => console.log(`Connected to ${host}:${port}`))
-		.on('data', (data) => {
+		.on("connect", () => console.log(`Connected to ${host}:${port}`))
+		.on("data", (data) => {
 			lineBuffer += data.toString();
 			let startIndex = 0;
 			let messageEndIndex = 0;
-			while ((messageEndIndex = lineBuffer.indexOf('\n', startIndex)) !== -1) {
-				let str = lineBuffer.slice(startIndex, messageEndIndex).replace('\n', '').replace('\r', '');
+			while ((messageEndIndex = lineBuffer.indexOf("\n", startIndex)) !== -1) {
+				let str = lineBuffer.slice(startIndex, messageEndIndex).replace("\n", "").replace("\r", "");
 				startIndex = messageEndIndex + 1;
-				const gIdx = str.indexOf('$GPZDA');
+				const gIdx = str.indexOf("$GPZDA");
 				// If this isnt a $GPZDA message, skip it
 				if (gIdx === -1) continue;
 
@@ -51,6 +51,6 @@ const start = async () => {
 admin
 	.check()
 	.then((hasAdminPrivs) => {
-		if (!hasAdminPrivs) exitWithError('NMEATCPTime requires admin privileges to run! Exiting...');
+		if (!hasAdminPrivs) exitWithError("NMEATCPTime requires admin privileges to run! Exiting...");
 	})
 	.then(start);
